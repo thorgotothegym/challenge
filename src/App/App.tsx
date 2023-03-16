a2f5bed9726b5e56e83e6f2eb3445479abf01a56
Aleix Vidal <al3ix.vidal@gmail.com>
2023-03-16 15:46:15 +0100

    feat: split styles

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,33 +1,30 @@
-import React from "react"
+import React from "react";
 
-import { Weather } from "../components/Weather"
+import { Weather } from "../components/Weather";
 
-import type { FC } from "react"
+import "./app.css";
 
-import "./app.css"
+export const App = (): JSX.Element => {
+  const [input, setInput] = React.useState(null as unknown as string);
 
-export const App: FC = () => {
-    const [ input, setInput ] = 
-        React.useState( null as unknown as string )
+  const [city, setCity] = React.useState(null as unknown as string);
 
-    const [ city, setCity ] =
-        React.useState( null as unknown as string )
+  const doStuff = (event: any) => setInput(event.target.value);
 
-    const doStuff = ( event: any ) => 
-        setInput( event.target.value )
+  console.log(input);
 
-    console.log( input )
-    
-    return  <>
-                <input 
-                    role="search"
-                    type="text" 
-                    value={ input as unknown as string } 
-                    onChange={ doStuff }  />
+  return (
+    <>
+      <input
+        role="search"
+        type="text"
+        value={input as unknown as string}
+        onChange={doStuff}
+      />
 
-                <button onClick={ () => setCity( input ) } >
-                    Show Weather
-                </button>
+      <button onClick={() => setCity(input)}>Show Weather</button>
 
-                < Weather city={ city } />
-            </> }
+      <Weather city={city} />
+    </>
+  );
+};
--- src/components/Weather/index.tsx
+++ src/components/Weather/index.tsx
@@ -1,47 +1,56 @@
-import React from "react"
+import React from "react";
+import "./index.css";
 
-const Weather = ( props: any ) => {
-    const [ info, setInfo ] = React.useState( {
-        city: null,
-        temp: null,
-        icon: null, } )
+const Weather = (props: any) => {
+  const [info, setInfo] = React.useState({
+    city: null,
+    temp: null,
+    icon: null,
+  });
 
-    console.log( "info" + JSON.stringify( info ) )
+  console.log("info" + JSON.stringify(info));
 
-    const isPending = React.useRef( false )
+  const isPending = React.useRef(false);
 
-    React.useLayoutEffect( () => {
-        if ( props.city !== null || isPending.current === false ) {
+  React.useLayoutEffect(() => {
+    if (props.city !== null || isPending.current === false) {
+      isPending.current = true;
 
-        isPending.current = true
+      fetch(
+        `https://api.openweathermap.org/geo/1.0/direct?q=${props.city}&appid=4c4f0b1876954338598a7be96c66527b`
+      )
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
 
-        fetch( `https://api.openweathermap.org/geo/1.0/direct?q=${ props.city }&appid=4c4f0b1876954338598a7be96c66527b` )
-            .then( res => res.json() )
-            .then( data => {
-                console.log( data )
+          fetch(
+            `https://api.openweathermap.org/data/2.5/weather?lat=${data[0].lat}&lon=${data[0].lon}&units=metric&appid=4c4f0b1876954338598a7be96c66527b`
+          )
+            .then((res) => res.json())
+            .then((data) => {
+              console.log(data);
 
-                fetch( `https://api.openweathermap.org/data/2.5/weather?lat=${ data[ 0 ].lat }&lon=${ data[ 0 ].lon }&units=metric&appid=4c4f0b1876954338598a7be96c66527b` )
-                    .then( res => res.json() )
-                    .then( data => {
-                        console.log( data )
-                        
-                        setInfo( {
-                            city: data.name,
-                            temp: data.main.temp,
-                            icon: `https://openweathermap.org/img/wn/${ data.weather[ 0 ].icon }@2x.png` as any, } ) 
-                        
-                        isPending.current = false } ) } ) } }, [ props.city ] )
+              setInfo({
+                city: data.name,
+                temp: data.main.temp,
+                icon: `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png` as any,
+              });
 
-    return  <>
-                <h1>
-                    { info.city }
-                </h1>
+              isPending.current = false;
+            });
+        });
+    }
+  }, [props.city]);
 
-                <p>
-                    { info.temp && ~~ info.temp } Celcius
-                </p>
+  return (
+    <>
+      <h1>{info.city}</h1>
 
-                <img src={ info.icon as unknown as string } alt="Icon" />
-            </> }
+      <p>{info.temp && ~~info.temp} Celcius</p>
 
-export { Weather }
+      <img src={info.icon as unknown as string} alt="Icon" />
+    </>
+  );
+};
+
+export { Weather };
