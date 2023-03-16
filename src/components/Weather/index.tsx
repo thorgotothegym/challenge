b804ccc8372b2197826978766beab3f5b411e4ec
Aleix Vidal <al3ix.vidal@gmail.com>
2023-03-16 16:24:51 +0100

    feat: remove logic and add entity and display data in the component

--- src/components/Weather/index.tsx
+++ src/components/Weather/index.tsx
@@ -1,54 +1,43 @@
 import React from "react";
-import "./index.css";
 
-export const Weather = (props: any) => {
+interface WeatherProps {
+  city: {
+    name: string;
+    id?: number;
+    code?: number;
+    timezone?: number;
+    coord?: {
+      lon?: number;
+      lat?: number;
+    };
+    weather?: {
+      description?: string;
+      icon: string;
+      id?: number;
+      main?: string;
+    };
+  };
+}
+
+export const Weather = (props: WeatherProps) => {
+  console.log("HELLO PROPS", props);
   const [info, setInfo] = React.useState({
     city: null,
     temp: null,
     icon: null,
   });
 
-  console.log("info" + JSON.stringify(info));
-
-  const isPending = React.useRef(false);
-
-  React.useLayoutEffect(() => {
-    if (props.city !== null || isPending.current === false) {
-      isPending.current = true;
-
-      fetch(
-        `https://api.openweathermap.org/geo/1.0/direct?q=${props.city}&appid=4c4f0b1876954338598a7be96c66527b`
-      )
-        .then((res) => res.json())
-        .then((data) => {
-          console.log(data);
-
-          fetch(
-            `https://api.openweathermap.org/data/2.5/weather?lat=${data[0].lat}&lon=${data[0].lon}&units=metric&appid=4c4f0b1876954338598a7be96c66527b`
-          )
-            .then((res) => res.json())
-            .then((data) => {
-              console.log(data);
-
-              setInfo({
-                city: data.name,
-                temp: data.main.temp,
-                icon: `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png` as any,
-              });
-
-              isPending.current = false;
-            });
-        });
-    }
-  }, [props.city]);
-
   return (
     <div data-testid="weatherID">
-      <h1>{info.city}</h1>
+      <h1>{props.city.name}</h1>
+      <ul>
+        <li>Long: {props?.city?.coord?.lon}</li>
+        <li>Lat: {props?.city?.coord?.lat}</li>
+      </ul>
 
       <p>{info.temp && ~~info.temp} Celcius</p>
 
-      <img src={info.icon as unknown as string} alt="Icon" />
+      <img src={props.city.weather?.icon} alt="Icon" />
     </div>
   );
 };
