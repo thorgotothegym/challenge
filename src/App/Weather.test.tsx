2aa9b8468ee72a215b9dda4af041ec8fb96026cf
Aleix Vidal <al3ix.vidal@gmail.com>
2023-03-16 15:41:02 +0100

    feat: remove unnecesary code

--- src/App/Weather.test.tsx
+++
@@ -1,8 +0,0 @@
-import { render } from "@testing-library/react"
-import { Weather } from "./Weather"
-
-describe( "Weather", () => {
-    it( "Renders Component According To Snapshot", () => {
-        const { container } = render( <Weather /> )
-        
-        expect( container ).toMatchSnapshot() } ) } )
