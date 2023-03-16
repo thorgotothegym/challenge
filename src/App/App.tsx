c4ac2ca516dcd5f4d60d94a1b6f50853b5a49d87
Aleix Vidal <al3ix.vidal@gmail.com>
2023-03-16 16:24:04 +0100

    feat: logic is implemented for default data and api calls

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,30 +1,61 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
 import { Weather } from "../components/Weather";
+import { ByCityReponse } from "../entities";
+import { getByCity, getByLatAndLon } from "../services";
 
 import "./app.css";
 
-export const App = (): JSX.Element => {
-  const [input, setInput] = React.useState(null as unknown as string);
+export const App = () => {
+  const [input, setInput] = useState<string>("");
 
-  const [city, setCity] = React.useState(null as unknown as string);
+  const [city, setCity] = useState<string>("Gortfadda");
+  const [byCity, setByCity] = useState<ByCityReponse>({
+    lat: 53.3201094,
+    lon: -8.567809712252107,
+    name: "Gortfadda",
+  });
+  const [loading, setLoading] = useState<boolean>(true);
+  const [data, setData] = useState<any>();
 
-  const doStuff = (event: any) => setInput(event.target.value);
+  const handleCity = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInput(event.target.value);
+    getData(event.target.value);
+  };
 
-  console.log(input);
+  const getData = async (city: string): Promise<void> => {
+    try {
+      const responseByCity = await getByCity(city);
+      const body = await responseByCity;
+
+      const responseByLatAndLon = await getByLatAndLon(
+        body[0].lat,
+        body[0].lon
+      );
+      const body2 = await responseByLatAndLon;
+      setByCity({ ...byCity, ...body2 });
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getData(city);
+  }, []);
 
   return (
     <>
       <input
         role="search"
         type="text"
-        value={input as unknown as string}
-        onChange={doStuff}
+        onChange={(event) => {
+          handleCity(event);
+        }}
       />
 
       <button onClick={() => setCity(input)}>Show Weather</button>
-
-      <Weather city={city} />
+      <Weather city={byCity} />
     </>
   );
 };
